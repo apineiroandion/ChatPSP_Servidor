2cfe298bb6bbe9a67c6dfa0fb0645d924eff4fd0
apineiroandion <apineiroandion@danielcastelao.org>
2025-02-13 10:20:36 +0100

    inicio de la conexion

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private final int MAX_USERS = 10;
+    private final List<Conexion> users = new ArrayList<>();
+
+    public Controller() {
+    }
+
+    public void start() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Servidor iniciado");
+            while (true) {
+                System.out.println("Esperando conexiones");
+                if (users.size() < MAX_USERS) {
+                    Conexion conexion = new Conexion(serverSocket.accept());
+                    users.add(conexion);
+                    conexion.start();
+                }else {
+                    System.out.println("Sala llena");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
