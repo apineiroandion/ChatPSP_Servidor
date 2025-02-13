08dd509780ee7d3a1f40204d8b5cf1a909f23f50
apineiroandion <apineiroandion@danielcastelao.org>
2025-02-13 11:15:02 +0100

    signacion de usuario a la clase conexion

--- src/main/java/controller/Conexion.java
+++ src/main/java/controller/Conexion.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.User;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,14 +9,16 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Conexion extends Thread {
+    private User usuarioConectado;
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
     private Controller controller;
 
-    public Conexion(Socket socket, Controller controller) {
+    public Conexion(Socket socket, Controller controller, User usuarioConectado) {
         this.socket = socket;
         this.controller = controller;
+        this.usuarioConectado = usuarioConectado;
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
@@ -40,5 +44,45 @@ public class Conexion extends Thread {
             }
             controller.removeUser(this);
         }
+    }
+
+    public User getUsuarioConectado() {
+        return usuarioConectado;
+    }
+
+    public void setUsuarioConectado(User usuarioConectado) {
+        this.usuarioConectado = usuarioConectado;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public BufferedReader getIn() {
+        return in;
+    }
+
+    public void setIn(BufferedReader in) {
+        this.in = in;
+    }
+
+    public PrintWriter getOut() {
+        return out;
+    }
+
+    public void setOut(PrintWriter out) {
+        this.out = out;
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
     }
 }
